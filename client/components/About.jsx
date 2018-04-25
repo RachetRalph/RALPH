b20ec09dec5a65f3a7eb3f87b9e932bc5b9341e3
RachetRalph <ralphandrade@zoho.com>
2018-04-25 09:34:58 -0700

    Added style & background fixes

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import {Row, Col, Image} from 'react-bootstrap';
+import "../css/about.css";
 
 class About extends Component {
   render() {
     return (
       <div>
         <h2>ABOUT</h2>
+        <div className="ralph">
+          <Row>
+            <Col xs={6} md={4}>
+              <Image src="/ralph.png" circle />
+            </Col>
+          </Row>
+        </div>
         <p>
           Cras facilisis urna ornare ex volutpat, et convallis erat elementum.
           Ut aliquam, ipsum vitae gravida suscipit, metus dui bibendum est, eget
@@ -12,7 +21,13 @@ class About extends Component {
           molestie. Cum sociis natoque penatibus et magnis dis parturient
           montes, nascetur ridiculus mus.
         </p>
-
+        <p>
+          Cras facilisis urna ornare ex volutpat, et convallis erat elementum.
+          Ut aliquam, ipsum vitae gravida suscipit, metus dui bibendum est, eget
+          rhoncus nibh metus nec massa. Maecenas hendrerit laoreet augue nec
+          molestie. Cum sociis natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
         <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
       </div>
     );
--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "../css/Contact.css";
+
 
 class Contact extends Component {
   render() {
